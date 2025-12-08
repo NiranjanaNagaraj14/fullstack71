eafc2c37d1217ecd47a43ab2bac92b1871d69386
NiranjanaNagaraj14 <nagarajrs5544@gmail.com>
2025-12-08 00:16:27 -0800

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import SummerCampRegistration from "./FSWD/Sample/SummerCampRegistration";
+
+function App() {
+  return <SummerCampRegistration />;
+}
+
+//import Welcome from "./FSWD/Sample/Welcome";
+//export default function App()
+//{
+  //return(<Welcome/>);
+//}
+
+
+//import PropsExample from "./FSWD/Sample/PropsExample";
+//export default function App()
+//{
+  //return<PropsExample title="Full Stack Web Development" name="Welcome to Web Development"/>;
+//}
+
+
+//import StateExample from "./FSWD/Sample/StateExample";
+//export default function App()
+//{
+  //return< StateExample/>;
+//}
+
+
+import StudentForm from "./FSWD/Sample/Studentform";
+export default function App()
+{
+  return< StudentForm/>;
+}
+
+
